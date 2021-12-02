38619dd600cd025f7fdf7b2fda56415bc64468a2
Kushal Garg <kushalgarg2000@gmail.com>
2021-12-02 15:33:10 +0530

    Converted RNNT model to a class

--- rnnt/main.py
+++ rnnt/main.py
@@ -1,12 +1,12 @@
 from json import encoder
-
+import tensorflow as tf
 from tensorflow import keras
 from preprocess import logmelspectogram
 from preprocess.slice import create_slices
 from config.config import speech_config
 import numpy as np
 
-from model.rnnt import rnnt
+from model.rnnt import RNNTransducer
 
 mel_spec = logmelspectogram.get("/home/kushal/Desktop/RNN-T/music.mp3")
 
@@ -16,8 +16,27 @@ dataset = np.array(object=object)
 np.append(dataset,mel_spec)
 print(np.array([mel_spec]).shape)
 
-model  = rnnt()
-model.compile(optimizer="adam",loss="mse")
+# model  = rnnt()
+# model.compile(optimizer="adam",loss="mse")
 
-print(model.summary())
-keras.utils.plot_model(model, "my_first_model.png")
+# print(model.summary())
+from config.config import model_config, speech_config
+
+model = RNNTransducer(
+  B=model_config["batch_size"],
+  T=model_config["timesteps"],
+  U=model_config["label_length"],
+  V=model_config["vocab_size"],
+)
+model.compile(optimizer="adam",loss="mse")
+print(model.build(input_shape=(None,3,80)))
+print(model._summary())
+
+# keras.utils.plot_model(model, "my_first_model.png")
+
+# converter = tf.lite.TFLiteConverter.from_keras_model(model)
+# tflite_model = converter.convert()
+
+# # Save the model.
+# with open('model.tflite', 'wb') as f:
+#   f.write(tflite_model)
--- rnnt/model/rnnt.py
+++ rnnt/model/rnnt.py
@@ -1,18 +1,91 @@
 from tensorflow.keras.layers import Dense, Flatten
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Concatenate
-from rnnt.model.encoder import encoder
-from rnnt.model.decoder import decoder
 import tensorflow as tf
+from tensorflow import keras
+from config.config import model_config, speech_config
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Embedding, RNN, LayerNormalization
+from tensorflow.keras import Input
 
-def rnnt():
-    enc = encoder()
-    dec = decoder()
-    joint_inp = (
-        tf.expand_dims(dec.output, axis=2) +                 # [B, T, V] => [B, T, 1, V]
-        tf.expand_dims(enc.output, axis=1)              # [B, U, V] => [B, 1, U, V]
-    )
-    merged = Dense(28, activation="tanh")(joint_inp)
-    dec.layers[0](merged)
+enc_config = model_config["encoder"]
+dec_config = model_config["decoder"]
 
-    return Model(inputs=[enc.input,dec.input],outputs=[merged], name="RNN-Transducer")
+class RNNTransducer(keras.Model):
+    def __init__(self,B,T,U,V,):
+        super(RNNTransducer,self).__init__()
+        self.B = lambda : B
+        self.T = lambda : T
+        self.U = lambda : U
+        self.V = lambda : V
+
+        self.enc = self.encoder()
+        self.dec = self.decoder()
+
+        print("TYPEEE",type(self.dec.input_shape),self.dec.input_shape)
+
+        prev_pred_init = tf.zeros_initializer()
+        self.prev_prediction = tf.Variable(
+            initial_value=prev_pred_init(shape=self.dec.input_shape, dtype="float32"), trainable=False
+        )
+
+        self.joint = Dense(self.V(), activation="tanh")     
+    
+    def _summary(self):
+        self.summary()
+        self.enc.summary()
+        self.dec.summary()
+
+    def call(self,inputs):
+        encoded_inp = self.enc(inputs)
+        decoded_prev = self.dec(self.prev_prediction)
+
+        joint_inp = (
+            tf.expand_dims(encoded_inp, axis=2) +                 # [B, T, V] => [B, T, 1, V]
+            tf.expand_dims(decoded_prev, axis=1)
+        )
+        print("JOINT INPut SHAPE",joint_inp.shape)
+        output = self.joint(joint_inp)
+        print("OUPUTSHAPE",output.shape)
+        self.prev_prediction = output
+
+        return output
+
+    def encoder(self):
+        num_layers = enc_config["num_layers"]
+        lstm_hidden_units = enc_config["lstm_hidden_units"]
+        lstm_projection = enc_config["lstm_projection"]
+        feature_bins = speech_config["feature_bins"]
+
+        model = Sequential()
+        model._init_set_name("encoder")
+        model.add(Input(shape=[self.T(),feature_bins], batch_size=self.B(), dtype=tf.float32))
+        rnn_cell = lambda: tf.compat.v1.nn.rnn_cell.LSTMCell(lstm_hidden_units, num_proj=lstm_projection, dtype=tf.float32)
+
+        for i in range(num_layers):
+            model.add(RNN(rnn_cell(),return_sequences=True, name=f"LSTM_ENC_{i+1}"))
+
+        return model
+    
+
+    def decoder(self):
+    
+        num_layers = dec_config["num_layers"]
+        lstm_hidden_units = dec_config["lstm_hidden_units"]
+        lstm_projection = dec_config["lstm_projection"]
+    
+        
+        inp = Input(shape=[self.U(),self.V()], batch_size=self.B(), dtype=tf.float32)
+        rnn_cell = lambda: tf.compat.v1.nn.rnn_cell.LSTMCell(lstm_hidden_units, num_proj=lstm_projection, dtype=tf.float32)
+    
+        output = inp
+        output = RNN(
+            tf.compat.v1.nn.rnn_cell.LSTMCell(lstm_hidden_units, num_proj=lstm_projection, dtype=tf.float32),
+            return_sequences=True, 
+            name=f"LSTM_DEC_1")(output)
+    
+        for i in range(1,num_layers):
+            output = RNN(rnn_cell(),return_sequences=True, name=f"LSTM_DEC_{i+1}")(output)
+    
+        return Model(inputs=inp,outputs=output,name="decoder")
+
